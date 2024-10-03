13bb1d88f09b87286bbedef69cc0b1847bcd6597
Brody K <brody@glimmer.org>
2024-10-03 11:41:55 -0400

    dingo feature w tailwind

--- app/brodingo/page.tsx
+++ app/brodingo/page.tsx
@@ -1,3 +1,24 @@
 export default function Brodingo() {
-    return (<h1>Hello world</h1>)
+    return (
+<div class="h-screen flex justify-center items-center">  
+<figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img class="w-24 h-32 md:w-48 md:h-auto mx-auto" src="//brodingo.com/img/mo.jpg" alt="Beefcake" width="384" height="512"/>
+  <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <blockquote>
+      <p class="text-lg font-medium">
+        “Tailwind CSS is one of the ways to do CSS.”
+      </p>
+    </blockquote>
+    <figcaption class="font-medium">
+      <div class="text-sky-500 dark:text-sky-400">
+        Brodingo
+      </div>
+      <div class="text-slate-700 dark:text-slate-500">
+        Pretty Cool Guy, For Real
+      </div>
+    </figcaption>
+  </div>
+</figure>  
+</div>      
+    )
 }
